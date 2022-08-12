26c79ab8e4684255323726c0d12e2ce10e8d8547
武子秋 <wuziqiu666@outlook.com>
2022-08-12 18:09:05 +0800

    11

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Solution solution = new Solution();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int num = solution.add(a, b);
+        System.out.println(num);
+        in.close();
+        
 
     }
 }
 
 class Solution {
+    public int  add(int a, int b) {
+        return a + b;
+    }
 
 }
