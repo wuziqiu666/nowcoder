748bb2327370fe4ebb3c5f718367f2f43d9b9b8f
founSword <wuziqiu666@outlook.com>
2022-08-31 01:26:45 +0800

    Update Test2.java
    ans--
    
    rebase

--- src/Test2.java
+++ src/Test2.java
@@ -8,13 +8,10 @@ public class Test2 {
 
         for (int i = 0; i < n; i++) {
             ans += 2;
-<<<<<<< HEAD
             ans += 3;
-=======
             ans -= 2;
             ans += 1;
             ans--;
->>>>>>> 47449ab (Update Test2.java)
         }
         System.out.println(ans);
 
