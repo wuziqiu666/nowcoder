4527261d8f3f65d6eb2494b37758e9e6776779e3
武子秋 <wuziqiu666@outlook.com>
2022-08-31 16:35:15 +0800

    update Main

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,10 @@ class Solution {
 
 class Test2 {
     public static void main(String[] args) {
-        int ans = 0;
-        int n = 10;
-        System.out.println("hello");
-        for (int i = 0; i < n; i++) {
-            ans += 2;
-            System.out.println(ans);
-        }
-        System.out.println("out");
+        Scanner in = new Scanner(System.in);
+
+        Solution solution = new Solution();
+
+        in.close();
     }
 }
