349bf4e7752fd1905aa495c39f89fdbdbc7bd9ca
武子秋 <wuziqiu666@outlook.com>
2022-08-31 16:34:26 +0800

    templata Main
    rebase 2

--- src/Main.java
+++ src/Main.java
@@ -13,3 +13,16 @@ public class Main {
 class Solution {
     public void slove() {}
 }
+
+class Test2 {
+    public static void main(String[] args) {
+        int ans = 0;
+        int n = 10;
+        System.out.println("hello");
+        for (int i = 0; i < n; i++) {
+            ans += 2;
+            System.out.println(ans);
+        }
+        System.out.println("out");
+    }
+}
