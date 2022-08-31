3956e9a4b810a66e7c5ccbc6ac4ca5b944809296
武子秋 <wuziqiu666@outlook.com>
2022-08-31 16:34:26 +0800

    templata Main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int ans = 0;
+        int n = 10;
+        System.out.println("hello");
+        for (int i = 0; i < n; i++) {
+            ans += 2;
+            System.out.println(ans);
+        }
+        System.out.println("out");
+    }
+}
