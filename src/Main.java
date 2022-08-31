87abaed68c88422aa147449526d1dae204ea70d7
founSword <wuziqiu666@outlook.com>
2022-09-01 01:33:07 +0800

    Update Main.java
    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        11111111111111
         in.close();
     }
 }
