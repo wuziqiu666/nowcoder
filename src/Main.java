a123dcfaa50dce7f48f200ed9f7e8ee83b788889
武子秋 <wuziqiu666@outlook.com>
2022-09-01 01:53:41 +0800

    tmplate

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        in.close();
-    }
-}
+        int a = 100;
+        while (a > 21) {
+            a--;
+        }
+        System.out.println(a);
+        // Scanner in = new Scanner(System.in);
 
-class Test2 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        in.close();
+        // in.close();
     }
 }
