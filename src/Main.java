efd36899ab51cf8ccbd2fbc701ae6a7a835ef84b
武子秋 <wuziqiu666@outlook.com>
2022-08-31 16:35:15 +0800

    update Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
-public class Test2 {
+public class Main {
     public static void main(String[] args) {
-        int ans = 0;
-        int n = 10;
-        System.out.println("hello");
-        for (int i = 0; i < n; i++) {
-            ans += 2;
-            System.out.println(ans);
-        }
-        System.out.println("out");
+        Scanner in = new Scanner(System.in);
+
+        Solution solution = new Solution();
+
+        in.close();
     }
+}
+
+class Solution {
+    public void slove() {}
 }
