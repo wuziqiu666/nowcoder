6d4599426f2b8f5a61a9b85e98740ad124e11d18
武子秋 <wuziqiu666@outlook.com>
2022-09-14 02:12:06 +0800

    baidu

--- src/Main.java
+++ src/Main.java
@@ -3,54 +3,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
-        while (in.hasNext()) {
-            list.add(in.next());
+        int n = in.nextInt();
+        int[] food1 = new int[n];
+        int[] food2 = new int[n];
+        for(int i = 0; i < n; i++){
+            food1[i] = in.nextInt();
         }
-        int n = list.size();
-        int m = list.get(0).split(",").length;
-        int[][] nums = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String[] s = list.get(i).split(",");
-            for (int j = 0; j < m; j++) {
-                nums[i][j] = s[j].charAt(0) - '0';
-            }
+        for(int i = 0; i < n; i++){
+            food2[i] = in.nextInt();
         }
-        int ans = slove(nums);
-        System.out.println(ans);
+        Solution solution = new Solution();
+        System.out.println(solution.slove(food1, food2));
+
 
         in.close();
     }
 
-    public static int slove(int[][] nums) {
-        int ans = 0;
-        int n = nums.length;
-        int m = nums[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (nums[i][j] == 1) {
-                    dfs(nums, i, j);
-                    ans++;
-                }
+    static class Solution{
+        public long slove(int[] food1, int[] food2){
+            int n = food1.length;
+            long a = food1[0], b = food2[0];
+            for(int i = 1; i < n; i++){
+                long na = Math.max(a, food1[i] + b);
+                long nb = Math.max(b, food2[i] + a);
+                a = na;
+                b = nb;
             }
-        }
-        return ans;
-    }
-
-    public static void dfs(int[][] nums, int x, int y) {
-        int n = nums.length;
-        int m = nums[0].length;
-        nums[x][y] = 0;
-        int[][] nextArray = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-        for (int[] next : nextArray) {
-            int nx = x + next[0];
-            int ny = y + next[1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                if (nums[nx][ny] == 1) {
-                    dfs(nums, nx, ny);
-                }
-            }
+            return Math.max(a, b);
+            
         }
     }
 
 }
+
+
